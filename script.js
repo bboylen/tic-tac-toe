ffd848a866c7634009beced4e5ac6ef7786306db
Brian <bbbrianboylen@gmail.com>
2020-11-25 11:36:10 -0600

    added functions

--- script.js
+++ script.js
@@ -33,15 +33,33 @@ const gameController = (() => {
   const player1 = Player("X");
   const player2 = Player("O");
 
+  let player1Turn = true;
+
+  const switchTurn = () => {
+    player1Turn = !player1Turn;
+  }
+
+  const playMessage = () => {
+    let player = player1Turn ? "Player 1's (X)" : "Player 2's (O)";
+    console.log(`It is ${player} turn:`);
+    // message.innerHTML = message
+  }
+
   // start game
 
-  // turn 
+  const playTurn = () => {
+    // check victory ?
+    // add event listeners (only ones not clicked yet) ?
+    // on click find object of click 
+    // add mark to square
+    // switch whose turn
+    // play turn again?
+  }
 
   // check victory
 })();
 
 
+const displayController = (() => {
 
-// const displayController = (() => {
-
-// })();
+})();
